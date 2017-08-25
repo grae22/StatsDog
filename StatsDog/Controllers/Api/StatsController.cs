be204a24b9c29162569206962ea78d7df47686b9
GraemeB <graeme.bruschi@derivco.co.za>
2017-08-25 13:11:07 +0200

    Simple logging of received stats.

--- StatsDog/Controllers/Api/StatsController.cs
+++ StatsDog/Controllers/Api/StatsController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Web.Http;
 using StatsDog.Dtos;
 
 namespace StatsDog.Controllers.Api
@@ -11,6 +15,24 @@ namespace StatsDog.Controllers.Api
     [HttpPost]
     public IHttpActionResult AddRecord(StatsDto statsDto)
     {
+      var stringBuilder = new StringBuilder();
+
+      stringBuilder.Append("StatsEvent: |");
+      stringBuilder.Append($" {DateTime.Now:yyyy-MM-dd HH:mm:ss} |");
+      stringBuilder.Append($" {statsDto.ApplicationName.PadRight(64)} |");
+      stringBuilder.Append($" {statsDto.ApplicationVersion.PadRight(16)} |");
+      stringBuilder.Append($" {statsDto.SourceName.PadRight(128)} |");
+      stringBuilder.Append($" {statsDto.EventName.PadRight(32)} |");
+
+      string output = stringBuilder.ToString();
+
+      Debug.WriteLine(output);
+
+      using (var writer = File.AppendText(@"d:\dev\projects\statsdog\deploy\stats.txt"))
+      {
+        writer.WriteLine(output);
+      }
+
       return Ok();
     }
 
