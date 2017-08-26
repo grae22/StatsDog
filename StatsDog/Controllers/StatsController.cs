66aa0005860a841f63bedb860cd26ae3f619f1a8
GraemeB <grae22@gmail.com>
2017-08-26 18:45:18 +0200

    Stats displayed from db.

---
+++ StatsDog/Controllers/StatsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using StatsDog.Models;
+
+namespace StatsDog.Controllers
+{
+  public class StatsController : Controller
+  {
+    //-------------------------------------------------------------------------
+
+    private readonly ApplicationDbContext _applicationDbContext = new ApplicationDbContext();
+
+    //-------------------------------------------------------------------------
+
+    protected override void Dispose(bool disposing)
+    {
+      _applicationDbContext.Dispose();
+    }
+
+    //-------------------------------------------------------------------------
+    // GET: Stats
+
+    public ViewResult Index()
+    {
+      return View(_applicationDbContext.Stats);
+    }
+
+    //-------------------------------------------------------------------------
+  }
+}
--- StatsDog/Models/Stats.cs
+++ StatsDog/Models/Stats.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace StatsDog.Models
@@ -7,18 +8,23 @@ namespace StatsDog.Models
   {
     public int Id { get; set; }
 
+    [DisplayName("Application Name")]
     [StringLength(64)]
     public string ApplicationName { get; set; }
 
+    [DisplayName("Application Version")]
     [StringLength(16)]
     public string ApplicationVersion { get; set; }
 
+    [DisplayName("Source")]
     [StringLength(128)]
     public string SourceName { get; set; }
 
+    [DisplayName("Event")]
     [StringLength(32)]
     public string EventName { get; set; }
 
+    [DisplayName("Timestamp")]
     public DateTime Timestamp { get; set; } = DateTime.Now;
   }
 }
